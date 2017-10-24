3f1d88864793d5e888b2254ec94f2b22f593c826
dragon <9734699+Skt0Dragon@users.noreply.github.com>
2017-10-24 16:39:24 +0800

    Update index.js

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,1 +1,1 @@
-export default typeof(window) == 'undefined' ? 'default://cib/imgs/' : 'http://101.201.67.232:8090/pan/'
+export default typeof(window) == 'undefined' ? 'default://cib/imgs/' : 'http://101.201.67.232:8888/pan/'
